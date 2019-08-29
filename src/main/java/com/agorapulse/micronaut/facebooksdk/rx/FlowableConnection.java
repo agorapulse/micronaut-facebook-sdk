bc7af15daf28a517cb831a67565028025980c8d1
Vladimir Orany <vladimir@orany.cz>
2019-08-29 14:34:45 +0200

    flowable connection

---
+++ src/main/java/com/agorapulse/micronaut/facebooksdk/rx/FlowableConnection.java
@@ -0,0 +1,35 @@
+package com.agorapulse.micronaut.facebooksdk.rx;
+
+import com.restfb.Connection;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import io.reactivex.Emitter;
+import io.reactivex.Flowable;
+
+import java.util.List;
+
+import static io.reactivex.Flowable.generate;
+
+public class FlowableConnection {
+
+    private FlowableConnection() {
+        // disallow instantiation
+    }
+
+    public static <T> Flowable<T> create(FacebookClient client, String connection, Class<T> connectionType, Parameter... parameters) {
+        return generate(() -> null, (String nextPage, Emitter<List<T>> emitter) -> {
+            Connection<T> conn = nextPage == null ?
+                    client.fetchConnection(connection, connectionType, parameters) :
+                    client.fetchConnectionPage(nextPage, connectionType);
+
+            emitter.onNext(conn.getData());
+
+            if (conn.hasNext()) {
+                return conn.getNextPageUrl();
+            } else {
+                emitter.onComplete();
+                return null;
+            }
+        }).flatMap(Flowable::fromIterable);
+    }
+}
---
+++ src/test/groovy/com/agorapulse/micronaut/facebooksdk/rx/FlowableConnectionSpec.groovy
@@ -0,0 +1,50 @@
+package com.agorapulse.micronaut.facebooksdk.rx
+
+import com.restfb.Connection
+import com.restfb.DefaultFacebookClient
+import com.restfb.FacebookClient
+import com.restfb.exception.FacebookException
+import io.reactivex.Flowable
+import spock.lang.Specification
+
+class FlowableConnectionSpec extends Specification {
+
+    void 'automatic pagination with flowable connection'() {
+        given:
+            FacebookClient client = Spy(DefaultFacebookClient)
+            Connection<String> first = new Connection<>(client,  '{ "data" : ["one", "two", "three"], "paging" : { "next" : "https://example.com/foobar" } }', String)
+            Connection<String> second = new Connection<>(client,  '{ "data" : ["four", "five", "six"] }', String)
+
+
+            _ * client.fetchConnection('/foo', String) >> first
+            _ * client.fetchConnectionPage('https://example.com/foobar', String) >> second
+        when:
+            Flowable<String> flowable = FlowableConnection.create(client, '/foo', String)
+        then:
+            0 * _
+
+        when:
+            List<String> all = flowable.toList().blockingGet()
+
+        then:
+            all == ['one', 'two', 'three', 'four', 'five', 'six']
+    }
+
+    void 'error handling'() {
+        given:
+            FacebookClient client = Spy(DefaultFacebookClient)
+
+            _ * client.fetchConnection('/foo', String) >> { throw new FacebookException("error") { } }
+        when:
+            Flowable<String> flowable = FlowableConnection.create(client, '/foo', String)
+        then:
+            0 * _
+
+        when:
+            List<String> all = flowable.onErrorReturn { it.message }.toList().blockingGet()
+
+        then:
+            all == ['error']
+    }
+
+}
