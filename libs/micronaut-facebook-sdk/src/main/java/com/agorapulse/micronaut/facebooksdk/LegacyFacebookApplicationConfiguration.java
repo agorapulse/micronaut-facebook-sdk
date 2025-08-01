4b85484d53939439e56903ef949d5fc9b70dac37
musketyr <vladimir@orany.cz>
2025-08-01 12:09:07 +0200

    returned required secret for legacy

--- libs/micronaut-facebook-sdk/src/main/java/com/agorapulse/micronaut/facebooksdk/LegacyFacebookApplicationConfiguration.java
+++ libs/micronaut-facebook-sdk/src/main/java/com/agorapulse/micronaut/facebooksdk/LegacyFacebookApplicationConfiguration.java
@@ -24,6 +24,7 @@ import jakarta.inject.Named;
 
 @Named("grails")
 @ConfigurationProperties("grails.plugin.facebooksdk.app")
+@Requires(property = "grails.plugin.facebooksdk.app.secret")
 public class LegacyFacebookApplicationConfiguration extends AbstractFacebookApplicationConfiguration {
 
 }
